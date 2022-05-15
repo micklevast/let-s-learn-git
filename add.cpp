40bfb8fdae444c080ff8b29895c0ac4f04911b11
vishal-micklevast <indvis2000@gamil.com>
2022-05-15 16:35:50 +0530

    added

---
+++ add.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter two number for multiple operation:";
+    cin>>a>>b;
+    cout<<"Sum:"<<a<<"+"<<b<<"="<<a+b<<endl;
+
+}
