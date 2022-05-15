f65dd00f16f6e987b1f0873e42bf6df4ae87f8c9
vishal-micklevast <indvis2000@gamil.com>
2022-05-15 16:17:17 +0530

    initial commit

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter two number for multiple operation:";
+    cin>>a>>b;
+    cout<<"Sum:"<<a<<"+"<<b<<"="<<a+b<<endl;
+    cout<<"diff:"<<a<<"-"<<b<<"="<<a-b<<endl;
+    cout<<"mul:"<<a<<"x"<<b<<"="<<a*b<<endl;
+}
